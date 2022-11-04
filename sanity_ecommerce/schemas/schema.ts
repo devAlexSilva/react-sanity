8f1b372a8a8b037ad84a5a5d2e27df1c367591ff
Alex Silva <9apsilva@gmail.com>
2022-11-04 20:22:09 -0300

    feat: add banner sanity schema

--- sanity_ecommerce/schemas/schema.ts
+++ sanity_ecommerce/schemas/schema.ts
@@ -1,8 +1,9 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import products from './products'
+import banner from './banner'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([products]),
+  types: schemaTypes.concat([products, banner]),
 })
